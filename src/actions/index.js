247d3880c1010d54fde15a54b2b2acc216215410
sgosl001 <sgosl001@ucer.edu>
2022-04-21 14:41:14 -0700

    Remove dead code

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,33 +1,33 @@
-export const addItem = ( id, listID, text ) => ({
-    type: 'ADD_ITEM',
-    payload: { id, listID, text }
+export const addItem = (id, listID, text) => ({
+  type: 'ADD_ITEM',
+  payload: { id, listID, text },
 });
 
 export const sort = (
-    droppableIdStart,
-    droppableIdEnd,
-    droppableIndexStart,
-    droppableIndexEnd,
-    draggableId
+  droppableIdStart,
+  droppableIdEnd,
+  droppableIndexStart,
+  droppableIndexEnd,
+  draggableId
 ) => {
-    return {
-        type: 'DRAG_HAPPENED',
-        payload: {
-            droppableIdStart,
-            droppableIdEnd,
-            droppableIndexStart,
-            droppableIndexEnd,
-            draggableId
-        }
-    }
-}
+  return {
+    type: 'DRAG_HAPPENED',
+    payload: {
+      droppableIdStart,
+      droppableIdEnd,
+      droppableIndexStart,
+      droppableIndexEnd,
+      draggableId,
+    },
+  };
+};
 
-export const removeItem = ( id, listID, text ) => ({
-    type: 'REMOVE_ITEM',
-    payload: { id, listID, text }
+export const removeItem = (id, listID, text) => ({
+  type: 'REMOVE_ITEM',
+  payload: { id, listID, text },
 });
 
-export const editItem = ( id, listID, text ) => ({
-    type: 'EDIT_ITEM',
-    payload: { id, listID, text }
-})
+export const editItem = (id, listID, text) => ({
+  type: 'EDIT_ITEM',
+  payload: { id, listID, text },
+});
